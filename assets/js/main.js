5fc857e193196047e56e91837b49c582212576e8
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-01-24 10:34:09 -0500

    Update main.js to prevent the nav box from scrolling if window width is < 1000px

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,42 @@
 window.onload = function() {
-	if (window.innerWidth >= 600) {
-		function getScrollTop() {
-		  if (typeof window.scrollY !== 'undefined' ) {
-		    // Most browsers
-		    return window.scrollY;
-		  }
+	function scrollElement() {
+		if (window.innerWidth >= 1000) {
+			function getScrollTop() {
+			  if (typeof window.scrollY !== 'undefined' ) {
+			    // Most browsers
+			    return window.scrollY;
+			  }
 
-		  var d = document.documentElement;
-		  if (d.clientHeight) {
-		    // IE in standards mode
-		    return d.scrollTop;
-		  }
+			  var d = document.documentElement;
+			  if (d.clientHeight) {
+			    // IE in standards mode
+			    return d.scrollTop;
+			  }
 
-		  // IE in quirks mode
-		  return document.body.scrollTop;
-		}
+			  // IE in quirks mode
+			  return document.body.scrollTop;
+			}
 
-		window.onscroll = function() {
-		  var box = document.getElementById('box'),
-		      scroll = getScrollTop();
+			window.onscroll = function() {
+			  var box = document.getElementById('box'),
+			      scroll = getScrollTop();
 
-		  if (scroll <= 28) {
-		    box.style.top = "0px";
-		  }
-		  else {
-		    box.style.top = (scroll + 2) + "px";
-		  }
+			  if (scroll <= 28) {
+			    box.style.top = "0px";
+			  }
+			  else {
+			    box.style.top = (scroll + 2) + "px";
+			  }
+			};
+		} else {
+			window.onscroll = null;
+			var box = document.getElementById('box')
+			box.style.top = "0px";
 		};
 	};
+
+	scrollElement();
+	window.addEventListener('resize', scrollElement, false);
 	
 	document.getElementById("day").innerHTML = dayOfWeek();
 };
